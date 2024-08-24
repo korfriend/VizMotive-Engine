21f301c69fe0e4003d080e968e265e2c08521b00
korfriend <korfriend@gmail.com>
2024-08-24 17:55:14 +0900

    base skeleton for renderpath interfaces

---
+++ EngineCore/Common/RenderPath3D.cpp
@@ -0,0 +1,13 @@
+#include "RenderPath3D.h"
+
+namespace vz
+{
+	void RenderPath3D::Render() const
+	{
+		// to do
+
+
+		// calling the parent function when this render() ends
+		RenderPath2D::Render();
+	}
+}
