693286ca0ad62a7b629bd7bcb0170c81916b7d59
korfriend <korfriend@gmail.com>
2024-08-25 03:49:35 +0900

    components updates

---
+++ EngineCore/HighAPIs/VzEngineAPIs.cpp
@@ -0,0 +1,27 @@
+#include "VzEngineAPIs.h"
+#include "../Components/EComponents.h"
+#include "../Utils/Backlog.h"
+
+using namespace vz;
+
+namespace vzm
+{
+	VZRESULT InitEngineLib(const vzm::ParamMap<std::string>& arguments)
+	{
+		static bool initialized = false;
+		if (initialized)
+		{
+			backlog::post("Already initialized!", backlog::LogLevel::Warn);
+			return VZ_WARNNING;
+		}
+
+		//sceneManager.Initialize(arguments);
+
+		return VZ_OK;
+	}
+
+	VZRESULT DeinitEngineLib()
+	{
+		return VZ_OK;
+	}
+}
---
+++ EngineCore/HighAPIs/VzEngineAPIs.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "VzComponents.h"
+
+namespace vzm
+{
+	// This must be called before using engine APIs
+	//  - paired with DeinitEngineLib()
+	extern "C" API_EXPORT VZRESULT InitEngineLib(const vzm::ParamMap<std::string>& arguments = vzm::ParamMap<std::string>());
+	extern "C" API_EXPORT VZRESULT DeinitEngineLib();
+}
