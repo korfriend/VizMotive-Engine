60b0a7d738f0cf88b378b558f699f6a94e92503d
korfriend <korfriend@gmail.com>
2024-10-17 16:25:45 +0900

    * impl. GGeometryComponent::UpdateRenderData() * add Vertex datastructures to GGeometryComponent * impl. GGeometryComponent::UpdateStreamoutRenderData()

--- EngineCore/Components/GComponents.h
+++ EngineCore/Components/GComponents.h
@@ -105,9 +105,186 @@ namespace vz
 
 		uint32_t geometryOffset = 0; // (including # of parts)
 
+		graphics::IndexBufferFormat GetIndexFormat() const;
+		inline size_t GetIndexStride() const { return GetIndexFormat() == graphics::IndexBufferFormat::UINT32 ? sizeof(uint32_t) : sizeof(uint16_t); }
 		GBuffers* GetGBuffer(const size_t slot) { return (GBuffers*)parts_[slot].bufferHandle_.get(); }
 		void UpdateRenderData() override;
 		void DeleteRenderData() override;
+		void UpdateStreamoutRenderData();
+
+		struct Vertex_POS32
+		{
+			float x = 0;
+			float y = 0;
+			float z = 0;
+
+			constexpr void FromFULL(const XMFLOAT3& pos)
+			{
+				x = pos.x;
+				y = pos.y;
+				z = pos.z;
+			}
+			inline XMVECTOR LoadPOS() const
+			{
+				return XMVectorSet(x, y, z, 1);
+			}
+			constexpr XMFLOAT3 GetPOS() const
+			{
+				return XMFLOAT3(x, y, z);
+			}
+			static constexpr graphics::Format FORMAT = graphics::Format::R32G32B32_FLOAT;
+		};
+		struct Vertex_POS32W
+		{
+			float x = 0;
+			float y = 0;
+			float z = 0;
+			float w = 0;
+
+			constexpr void FromFULL(const XMFLOAT3& pos, uint8_t wind)
+			{
+				x = pos.x;
+				y = pos.y;
+				z = pos.z;
+				w = float(wind) / 255.0f;
+			}
+			inline XMVECTOR LoadPOS() const
+			{
+				return XMVectorSet(x, y, z, 1);
+			}
+			constexpr XMFLOAT3 GetPOS() const
+			{
+				return XMFLOAT3(x, y, z);
+			}
+			constexpr uint8_t GetWind() const
+			{
+				return uint8_t(w * 255);
+			}
+			static constexpr graphics::Format FORMAT = graphics::Format::R32G32B32A32_FLOAT;
+		};
+		graphics::Format positionFormat = Vertex_POS32::FORMAT; // can be modified (current version uses Vertex_POS32 as default)
+
+		struct Vertex_TEX
+		{
+			uint16_t x = 0;
+			uint16_t y = 0;
+
+			constexpr void FromFULL(const XMFLOAT2& uv, const XMFLOAT2& uv_range_min = XMFLOAT2(0, 0), const XMFLOAT2& uv_range_max = XMFLOAT2(1, 1))
+			{
+				x = uint16_t(math::InverseLerp(uv_range_min.x, uv_range_max.x, uv.x) * 65535.0f);
+				y = uint16_t(math::InverseLerp(uv_range_min.y, uv_range_max.y, uv.y) * 65535.0f);
+			}
+			static constexpr graphics::Format FORMAT = graphics::Format::R16G16_UNORM;
+		};
+		struct Vertex_UVS
+		{
+			Vertex_TEX uv0;
+			Vertex_TEX uv1;
+			static constexpr graphics::Format FORMAT = graphics::Format::R16G16B16A16_UNORM;
+		};
+		struct Vertex_BON
+		{
+			XMUINT4 packed = XMUINT4(0, 0, 0, 0);
+
+			constexpr void FromFULL(const XMUINT4& boneIndices, const XMFLOAT4& boneWeights)
+			{
+				// Note:
+				//	- Indices are packed at 20 bits which allow indexing >1 million bones per mesh
+				//	- Weights are packed at 12 bits which allow 4096 distinct values, this was tweaked to
+				//		retain good precision with a high bone count stanford bunny soft body simulation where regular 8 bit weights was not enough
+				packed.x = (boneIndices.x & 0xFFFFF) | ((uint32_t(boneWeights.x * 4095) & 0xFFF) << 20u);
+				packed.y = (boneIndices.y & 0xFFFFF) | ((uint32_t(boneWeights.y * 4095) & 0xFFF) << 20u);
+				packed.z = (boneIndices.z & 0xFFFFF) | ((uint32_t(boneWeights.z * 4095) & 0xFFF) << 20u);
+				packed.w = (boneIndices.w & 0xFFFFF) | ((uint32_t(boneWeights.w * 4095) & 0xFFF) << 20u);
+			}
+			constexpr XMUINT4 GetInd_FULL() const
+			{
+				return XMUINT4(packed.x & 0xFFFFF, packed.y & 0xFFFFF, packed.z & 0xFFFFF, packed.w & 0xFFFFF);
+			}
+			constexpr XMFLOAT4 GetWei_FULL() const
+			{
+				return XMFLOAT4(
+					float(packed.x >> 20u) / 4095.0f,
+					float(packed.y >> 20u) / 4095.0f,
+					float(packed.z >> 20u) / 4095.0f,
+					float(packed.w >> 20u) / 4095.0f
+				);
+			}
+		};
+		struct Vertex_COL
+		{
+			uint32_t color = 0;
+			static constexpr graphics::Format FORMAT = graphics::Format::R8G8B8A8_UNORM;
+		};
+		struct Vertex_NOR
+		{
+			int8_t x = 0;
+			int8_t y = 0;
+			int8_t z = 0;
+			int8_t w = 0;
+
+			void FromFULL(const XMFLOAT3& nor)
+			{
+				XMVECTOR N = XMLoadFloat3(&nor);
+				N = XMVector3Normalize(N);
+				XMFLOAT3 n;
+				XMStoreFloat3(&n, N);
+
+				x = int8_t(n.x * 127.5f);
+				y = int8_t(n.y * 127.5f);
+				z = int8_t(n.z * 127.5f);
+				w = 0;
+			}
+			inline XMFLOAT3 GetNOR() const
+			{
+				return XMFLOAT3(
+					float(x) / 127.5f,
+					float(y) / 127.5f,
+					float(z) / 127.5f
+				);
+			}
+			inline XMVECTOR LoadNOR() const
+			{
+				return XMVectorSet(
+					float(x) / 127.5f,
+					float(y) / 127.5f,
+					float(z) / 127.5f,
+					0
+				);
+			}
+			static constexpr graphics::Format FORMAT = graphics::Format::R8G8B8A8_SNORM;
+		};
+		struct Vertex_TAN
+		{
+			int8_t x = 0;
+			int8_t y = 0;
+			int8_t z = 0;
+			int8_t w = 0;
+
+			void FromFULL(const XMFLOAT4& tan)
+			{
+				XMVECTOR T = XMLoadFloat4(&tan);
+				T = XMVector3Normalize(T);
+				XMFLOAT4 t;
+				XMStoreFloat4(&t, T);
+				t.w = tan.w;
+
+				x = int8_t(t.x * 127.5f);
+				y = int8_t(t.y * 127.5f);
+				z = int8_t(t.z * 127.5f);
+				w = int8_t(t.w * 127.5f);
+			}
+			inline XMFLOAT4 GetTAN() const
+			{
+				return XMFLOAT4(
+					float(x) / 127.5f,
+					float(y) / 127.5f,
+					float(z) / 127.5f,
+					float(w) / 127.5f
+				);
+			}
+			static constexpr graphics::Format FORMAT = graphics::Format::R8G8B8A8_SNORM;
+		};
 
 		// CreateRaytracingRenderData
 		
