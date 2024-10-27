61c987d68b876a0772d8d04d53d637349a68edeb
korfriend <korfriend@gmail.com>
2024-10-27 15:43:37 +0900

    * impl. vertexcolorVS.hlsl * impl. meshPS.hlsl * impl. meshPS_debug.hlsl * impl. meshPS_simple.hlsl

--- EnginePlugins/RendererDX12/Shaders/compile.bat
+++ EnginePlugins/RendererDX12/Shaders/compile.bat
@@ -1,2 +1,7 @@
  dxc -T vs_6_0 ./VS/meshVS_debug.hlsl -spirv
  dxc -T vs_6_0 ./VS/meshVS_common.hlsl -spirv
+ dxc -T vs_6_0 ./VS/meshVS_simple.hlsl -spirv
+ dxc -T vs_6_0 ./VS/vertexcolorVS.hlsl -spirv
+
+ dxc -T ps_6_0 ./PS/meshPS_debug.hlsl -spirv
+ dxc -T ps_6_0 ./PS/meshPS_simple.hlsl -spirv
