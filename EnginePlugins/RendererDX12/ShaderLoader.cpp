ae23d7b7c841a310ee6e9a4a1467d1f8f252b26d
Jihun Song <wlgns1080@naver.com>
2025-01-22 16:22:21 +0900

    * add gs_offsetCS.hlsl
    - add gs_offsetCS.hlsl

--- EnginePlugins/RendererDX12/ShaderLoader.cpp
+++ EnginePlugins/RendererDX12/ShaderLoader.cpp
@@ -558,10 +558,10 @@ namespace vz::shader
 		
 			});
 
-		jobsystem::Execute(ctx, [](jobsystem::JobArgs args) { LoadShader(ShaderStage::CS, rcommon::shaders[CSTYPE_GS_GAUSSIAN_TOUCH_COUNT], "testCS.cso"); });
-		jobsystem::Execute(ctx, [](jobsystem::JobArgs args) { LoadShader(ShaderStage::CS, rcommon::shaders[CSTYPE_GS_GAUSSIAN_OFFSET], "testCS.cso"); });
-		jobsystem::Execute(ctx, [](jobsystem::JobArgs args) { LoadShader(ShaderStage::CS, rcommon::shaders[CSTYPE_GS_DUPLICATED_GAUSSIANS], "testCS.cso"); });
-		jobsystem::Execute(ctx, [](jobsystem::JobArgs args) { LoadShader(ShaderStage::CS, rcommon::shaders[CSTYPE_GS_SORT_DUPLICATED_GAUSSIANS], "testCS.cso"); });
+		jobsystem::Execute(ctx, [](jobsystem::JobArgs args) { LoadShader(ShaderStage::CS, rcommon::shaders[CSTYPE_GS_GAUSSIAN_TOUCH_COUNT], "gs_preprocessCS.cso"); });
+		jobsystem::Execute(ctx, [](jobsystem::JobArgs args) { LoadShader(ShaderStage::CS, rcommon::shaders[CSTYPE_GS_GAUSSIAN_OFFSET], "gs_offsetCS.cso"); });
+		//jobsystem::Execute(ctx, [](jobsystem::JobArgs args) { LoadShader(ShaderStage::CS, rcommon::shaders[CSTYPE_GS_DUPLICATED_GAUSSIANS], "testCS.cso"); });
+		//jobsystem::Execute(ctx, [](jobsystem::JobArgs args) { LoadShader(ShaderStage::CS, rcommon::shaders[CSTYPE_GS_SORT_DUPLICATED_GAUSSIANS], "testCS.cso"); });
 
 		jobsystem::Execute(ctx, [](jobsystem::JobArgs args) { LoadShader(ShaderStage::CS, rcommon::shaders[CSTYPE_DVR_DEFAULT], "dvrCS.cso"); });
 
